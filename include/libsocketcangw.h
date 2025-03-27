36bacb6c193fc2d1a48d86e07b45c877d02f4bdb
Naoto Yamaguchi <naoto.yamaguchi@aisin.co.jp>
2025-03-28 03:06:18 +0900

    Create add/delete function for CAN gateway configuration
    This patch create cangw_add_rule and cangw_delete_rule
    functions.  These support to modify CAN gateway configuration
    one by one.

---
+++ include/libsocketcangw.h
@@ -0,0 +1,55 @@
+/*
+ * libsocketcangw.h
+ *
+ * (C) 2025 Naoto Yamaguchi <naoto.yamaguchi@aisin.co.jp>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but without
+ * any warranty; without even the implied warranty of merchantability or fitness
+ * for a particular purpose. see the gnu lesser general public license for more
+ * details.
+ *
+ * you should have received a copy of the gnu lesser general public license
+ * along with this library; if not, write to the free software foundation, inc.,
+ * 59 temple place, suite 330, boston, ma 02111-1307 usa
+ */
+
+ #ifndef _socketcangw_netlink_h
+ #define _socketcangw_netlink_h
+ 
+ /**
+  * @file
+  * @brief API overview
+  */
+ #include <linux/can.h>
+ 
+ #ifdef __cplusplus
+ extern "C" {
+ #endif
+
+#define SOCKETCAN_GW_RULE_ECHO		(0x00000001U)
+#define SOCKETCAN_GW_RULE_FILTER	(0x00000002U)
+
+struct s_socketcan_gw_rule {
+	unsigned int src_ifindex;
+	unsigned int dst_ifindex;
+
+	unsigned int	options;
+
+	unsigned int	echo;
+	struct can_filter filter;
+ };
+typedef struct s_socketcan_gw_rule socketcan_gw_rule_t;
+ 
+
+int cangw_add_rule(socketcan_gw_rule_t *rule);
+int cangw_delete_rule(socketcan_gw_rule_t *rule);
+ 
+ #ifdef __cplusplus
+ }
+ #endif
+ 
+ #endif
---
+++ src/libcangw.c
@@ -0,0 +1,226 @@
+/* libcangw.c
+ *
+ * (C) 2025 Naoto Yamaguchi <naoto.yamaguchi@aisin.co.jp>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/**
+ * @file
+ * @brief Socket CAN gateway library
+ */
+
+#include <linux/can/gw.h>
+#include <sys/socket.h>
+
+#include "libsocketcan-utils.h"
+
+#include <libsocketcangw.h>
+
+struct s_request_data {
+	struct nlmsghdr nh;
+	struct rtcanmsg rtcan;
+	char buf[1500];
+};
+
+static int send_cangw_request(struct s_request_data *req)
+{
+	int result = 0;
+	int ret = -1;
+	int sock_fd = -1;
+	struct nlmsghdr *nlh = NULL;
+	struct nlmsgerr *rte = NULL;
+	struct sockaddr_nl nladdr;
+	unsigned char rxbuf[8192];
+
+	// Open netlink socket interface
+	sock_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
+	if (sock_fd < 0) {
+		result = -1;
+		goto do_return;
+	}
+
+	memset(&nladdr, 0, sizeof(nladdr));
+	nladdr.nl_family = AF_NETLINK;
+	nladdr.nl_pid    = 0;
+	nladdr.nl_groups = 0;
+
+	ret = sendto(sock_fd, req, req->nh.nlmsg_len, 0, (struct sockaddr*)&nladdr, sizeof(nladdr));
+	if (ret < 0) {
+		result = -1;
+		goto do_return;
+	}
+
+	memset(rxbuf, 0, sizeof(rxbuf));
+	ret = recv(sock_fd, &rxbuf, sizeof(rxbuf), 0);
+	if (ret < 0) {
+		result = -1;
+		goto do_return;
+	}
+
+	nlh = (struct nlmsghdr *)rxbuf;
+	if (nlh->nlmsg_type != NLMSG_ERROR) {
+		result = -2;
+		goto do_return;
+	}
+
+	rte = (struct nlmsgerr *)NLMSG_DATA(nlh);
+	if (rte->error < 0) {
+		result = -3;
+	}
+
+do_return:
+	if (sock_fd >= 0) {
+		close(sock_fd);
+	}
+
+	return result;
+}
+/**
+ * @ingroup intern
+ * @brief init_req_data - initialize for req data
+ *
+ * @param req pointer to s_request_data structure that is initialized by this function.
+ * @param flags value of the nlmsg_flags
+ * @param type value of the nlmsg_type
+ *
+ * Set a netlink request data from rule to req.
+ */
+static void init_req_data(struct s_request_data *req, unsigned short flags, unsigned short type)
+{
+	// Setup common message
+	memset(req, 0, sizeof(struct s_request_data));
+
+	req->nh.nlmsg_flags = flags;
+	req->nh.nlmsg_type  = type;
+	req->nh.nlmsg_len   = NLMSG_LENGTH(sizeof(struct rtcanmsg));
+	req->nh.nlmsg_seq   = 0;
+
+	req->rtcan.can_family  = AF_CAN;
+	req->rtcan.gwtype = CGW_TYPE_CAN_CAN;
+	req->rtcan.flags = 0;
+}
+
+/**
+ * @ingroup intern
+ * @brief operate_rule_options - operate to options of gw rule
+ *
+ * @param req pointer to s_request_data structure that is wrote the options.
+ * @param rule pointer to source data of the gw configuration rule
+ *
+ * Set a netlink request data from rule to req.
+ */
+static void operate_rule_options(struct s_request_data *req, socketcan_gw_rule_t *rule)
+{
+	if ((rule->options & SOCKETCAN_GW_RULE_ECHO) == SOCKETCAN_GW_RULE_ECHO) {
+		if (rule->echo == 1) {
+			req->rtcan.flags |= CGW_FLAGS_CAN_ECHO;
+		}
+	}
+
+	if ((rule->options & SOCKETCAN_GW_RULE_FILTER) == SOCKETCAN_GW_RULE_FILTER) {
+		addattr_l(&req->nh, sizeof(struct s_request_data), CGW_FILTER, &rule->filter, sizeof(struct can_filter));
+	}
+}
+
+/**
+ * @ingroup extern
+ * cangw_add_rule - add routing rule to can gateway
+ * @param rule rule data of the can gateway.
+ *
+ * @return 0 if success
+ * @return -1 if operation is failed
+ * @return -2 if linux does not support can gateway
+ * @return -3 if argument is invalid
+ */
+int cangw_add_rule(socketcan_gw_rule_t *rule)
+{
+	int result = 0;
+	int ret = -1;
+	struct s_request_data req;
+
+	if (rule == NULL) {
+		result = -3;
+		goto do_return;
+	}
+
+	// Setup common message
+	init_req_data(&req, (NLM_F_REQUEST | NLM_F_ACK), RTM_NEWROUTE);
+
+	if ((rule->src_ifindex == 0) || (rule->dst_ifindex == 0)) {
+		// invalid ifindex
+		result = -3;
+		goto do_return;
+	}
+	addattr_l(&req.nh, sizeof(req), CGW_SRC_IF, &rule->src_ifindex, sizeof(rule->src_ifindex));
+	addattr_l(&req.nh, sizeof(req), CGW_DST_IF, &rule->dst_ifindex, sizeof(rule->dst_ifindex));
+
+	// operate options
+	operate_rule_options(&req, rule);
+
+	ret = send_cangw_request(&req);
+	if (ret < 0) {
+		result = -1;
+		goto do_return;
+	}
+
+do_return:
+	return result;
+}
+
+/**
+ * @ingroup extern
+ * cangw_delete_rule - delete routing rule to can gateway
+ * @param rule rule data of the can gateway.
+ *
+ * @return 0 if success
+ * @return -1 if operation is failed
+ * @return -2 if linux does not support can gateway
+ * @return -3 if argument is invalid
+ */
+int cangw_delete_rule(socketcan_gw_rule_t *rule)
+{
+	int result = 0;
+	int ret = -1;
+	struct s_request_data req;
+
+	if (rule == NULL) {
+		result = -3;
+		goto do_return;
+	}
+
+	// Setup common message
+	init_req_data(&req, (NLM_F_REQUEST | NLM_F_ACK), RTM_DELROUTE);
+
+	if ((rule->src_ifindex == 0) || (rule->dst_ifindex == 0)) {
+		// invalid ifindex
+		result = -3;
+		goto do_return;
+	}
+	addattr_l(&req.nh, sizeof(req), CGW_SRC_IF, &rule->src_ifindex, sizeof(rule->src_ifindex));
+	addattr_l(&req.nh, sizeof(req), CGW_DST_IF, &rule->dst_ifindex, sizeof(rule->dst_ifindex));
+
+	// operate options
+	operate_rule_options(&req, rule);
+
+	ret = send_cangw_request(&req);
+	if (ret < 0) {
+		result = -1;
+		goto do_return;
+	}
+
+do_return:
+	return result;
+}
