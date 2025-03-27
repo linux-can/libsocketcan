7c73010f843a24528a01b454ab37c0a27bd1e449
Naoto Yamaguchi <naoto.yamaguchi@aisin.co.jp>
2025-03-28 03:07:02 +0900

    Adding a test code for add/delete function
    This patch add test code to test to cangw_add_rule and
    cangw_delete_rule.

---
+++ src/test/gwtest.c
@@ -0,0 +1,44 @@
+#include "../libsocketcan-utils.c"
+#include "../libcangw.c"
+
+
+int cangw_add_rule_test(void)
+{
+	int ret = -1;
+	socketcan_gw_rule_t gw_rule;
+
+	memset(&gw_rule, 0, sizeof(gw_rule));
+
+	gw_rule.src_ifindex = if_nametoindex("vcan0");
+	gw_rule.dst_ifindex = if_nametoindex("vxcan0");
+
+	gw_rule.options |= SOCKETCAN_GW_RULE_ECHO;
+	gw_rule.echo = 0;
+
+	gw_rule.options |= SOCKETCAN_GW_RULE_FILTER;
+	gw_rule.filter.can_id = 0x3C0;
+	gw_rule.filter.can_mask = 0xff0;
+
+	ret = cangw_add_rule(&gw_rule);
+	if (ret < 0) {
+		fprintf(stdout,"cangw_add_rule is failed ret = %d\n",ret);
+		return -1;
+	}
+
+	ret = cangw_delete_rule(&gw_rule);
+	if (ret < 0) {
+		fprintf(stdout,"cangw_delete_rule is failed ret = %d\n",ret);
+		return -2;
+	}
+
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	int ret = -1;
+	ret = cangw_add_rule_test();
+	fprintf(stdout,"ret = %d\n",ret);
+
+	return 0;
+}
